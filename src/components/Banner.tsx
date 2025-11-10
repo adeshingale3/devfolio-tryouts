42ca4fe4e5261b5b5136c2f571f2c47c7f8e40f7
adeshingale3 <adeshingale600@gmail.com>
2025-11-10 11:48:49 +0530

    responsive banner

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,21 +1,16 @@
 
 const Banner = () => {
   return (
-    <div className="relative w-auto h-[300px] overflow-hidden px-40 bg-white">
-
+    <div className="relative w-full h-[200px] sm:h-[250px] md:h-[300px] lg:h-[400px] overflow-hidden px-4 sm:px-10 md:px-20 lg:px-40 bg-white flex items-center justify-center">
       <img
         src="https://i.pinimg.com/1200x/c4/37/2c/c4372c63d4349e122ed1f5613ccd8adb.jpg"
         alt="banner"
         className="w-full h-full object-cover"
       />
 
-      {/* Optional overlay for dim or gradient effect */}
-
+      {/* Optional gradient overlay for contrast */}
     </div>
+  );
+};
 
-
-
-  )
-}
-
-export default Banner
+export default Banner;
